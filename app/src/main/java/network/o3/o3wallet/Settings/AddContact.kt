9bf8de5cff325b0f514bc0d2a09d42772a51514d
Andrei Terentiev <drei@Andreis-MacBook-Pro.local>
2017-12-19 11:51:30 -0800

    qr scanning watch address and contacts

--- app/src/main/java/network/o3/o3wallet/Settings/AddContact.kt
+++ app/src/main/java/network/o3/o3wallet/Settings/AddContact.kt
@@ -1,7 +1,9 @@
 package network.o3.o3wallet.Settings
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.widget.Button
 import android.widget.EditText
 import network.o3.o3wallet.PersistentStore
@@ -10,6 +12,8 @@ import network.o3.o3wallet.API.NEO.NeoNodeRPC
 import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import net.glxn.qrgen.android.QRCode
 
 class AddContact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,16 @@ class AddContact : AppCompatActivity() {
         val nickNameField = findViewById<EditText>(R.id.NickNameField)
         val addressField = findViewById<EditText>(R.id.AddressField)
         val button = findViewById<Button>(R.id.AddButton)
+
+        val actionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        actionButton.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+            integrator.setPrompt("Scan the QR code of the address you want to save")
+            integrator.setOrientationLocked(false)
+            integrator.initiateScan()
+        }
 
         button.setOnClickListener {
             val errorAlert = AlertDialog.Builder(this).create()
@@ -35,6 +49,17 @@ class AddContact : AppCompatActivity() {
                 } else {
                     PersistentStore.addContact(addressField.text.toString(), nickNameField.text.toString())
                 }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null ) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                findViewById<EditText>(R.id.AddressField).setText(result.contents)
             }
         }
     }
--- app/src/main/java/network/o3/o3wallet/Settings/AddWatchAddress.kt
+++ app/src/main/java/network/o3/o3wallet/Settings/AddWatchAddress.kt
@@ -1,10 +1,14 @@
 package network.o3.o3wallet.Settings
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AlertDialog
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 import network.o3.o3wallet.PersistentStore
 import network.o3.o3wallet.R
 import network.o3.o3wallet.API.NEO.*
@@ -17,6 +21,16 @@ class AddWatchAddress : AppCompatActivity() {
         val nickNameField = findViewById<EditText>(R.id.NickNameField)
         val addressField = findViewById<EditText>(R.id.AddressField)
         val button = findViewById<Button>(R.id.AddButton)
+
+        val actionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        actionButton.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+            integrator.setPrompt("Scan the QR code of the address you want to save")
+            integrator.setOrientationLocked(false)
+            integrator.initiateScan()
+        }
 
         button.setOnClickListener {
             val errorAlert = AlertDialog.Builder(this).create()
@@ -36,6 +50,16 @@ class AddWatchAddress : AppCompatActivity() {
                 }
             }
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null ) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                findViewById<EditText>(R.id.AddressField).setText(result.contents)
+            }
+        }
     }
 }
