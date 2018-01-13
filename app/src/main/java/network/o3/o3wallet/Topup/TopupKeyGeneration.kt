56808fc34f65f474bde88dde9d2c33d86dd66262
Andrei Terentiev <drei@Andreis-MacBook-Pro.local>
2018-01-13 15:37:41 +0900

    continue

--- app/src/main/java/network/o3/o3wallet/Topup/TopupKeyGeneration.kt
+++ app/src/main/java/network/o3/o3wallet/Topup/TopupKeyGeneration.kt
@@ -2,6 +2,9 @@ package network.o3.o3wallet.Topup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.topup_activity_topup_key_generation.*
 import network.o3.o3wallet.R
 
 class TopupKeyGeneration : AppCompatActivity() {
@@ -9,5 +12,15 @@ class TopupKeyGeneration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.topup_activity_topup_key_generation)
+
+        Handler().postDelayed({ showCompletion() }, 3000)
     }
+
+        fun showCompletion() {
+            encryptingProgress.visibility = View.INVISIBLE
+            encryptionProgressTextView.text = resources.getString(R.string.encryption_done)
+            lockImage.visibility = View.VISIBLE
+            encryptionInfoTextView.visibility = View.VISIBLE
+            continueEncryptionButton.visibility = View.VISIBLE
+        }
 }
