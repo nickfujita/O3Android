e782093d893200e33b0b3cf93affdd4b0fcea2f0
SaltySkip <terentiev.andrei@gmail.com>
2018-06-01 11:55:27 +0900

    add-pricing data alert

--- app/src/main/java/network/o3/o3wallet/Portfolio/AssetListAdapter.kt
+++ app/src/main/java/network/o3/o3wallet/Portfolio/AssetListAdapter.kt
@@ -13,7 +13,10 @@ import network.o3.o3wallet.*
 import network.o3.o3wallet.API.NEO.AccountAsset
 import network.o3.o3wallet.API.O3.Portfolio
 import network.o3.o3wallet.API.O3Platform.TransferableAsset
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.find
 import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.yesButton
 import java.text.NumberFormat
 
 /**
@@ -81,6 +84,27 @@ class AssetListAdapter(context: Context, fragment: HomeFragment): BaseAdapter() 
         assetTotalValueView.text = asset.totalValue.formattedCurrencyString(referenceCurrency)
         assetPercentChangeView.text = asset.percentChange.formattedPercentString()
 
+        if (asset.assetPrice == 0.0 && portfolio?.price?.get(assets.get(position).symbol)?.averageUSD != null) {
+            assetPercentChangeView.visibility = View.GONE
+            assetPriceView.visibility = View.GONE
+            view.find<TextView>(R.id.pricingNotAvailableTextView).visibility = View.VISIBLE
+            view.setOnClickListener {
+                mfragment.activity?.alert (
+                    mContext.getString(R.string.PORTFOLIO_pricing_not_available_description)) {
+                    yesButton { mContext.getString(R.string.ALERT_OK_Confirm_Button) }
+                }?.show()
+            }
+        } else {
+            assetPercentChangeView.visibility = View.VISIBLE
+            assetPriceView.visibility = View.VISIBLE
+            view.find<TextView>(R.id.pricingNotAvailableTextView).visibility = View.GONE
+            view.setOnClickListener {
+                val intent = Intent(mfragment.activity, AssetGraph::class.java)
+                intent.putExtra("SYMBOL", asset.assetName.capitalize())
+                mfragment.activity?.startActivity(intent)
+            }
+        }
+
         if (asset.percentChange < 0) {
             assetPercentChangeView.setTextColor(ContextCompat.getColor(mContext, R.color.colorLoss))
         } else {
@@ -90,14 +114,6 @@ class AssetListAdapter(context: Context, fragment: HomeFragment): BaseAdapter() 
         var formatter = NumberFormat.getNumberInstance()
         formatter.maximumFractionDigits = assets[position].decimals
         assetAmountView.text = formatter.format(asset.assetAmount)
-
-
-
-        view.setOnClickListener {
-            val intent = Intent(mfragment.activity, AssetGraph::class.java)
-            intent.putExtra("SYMBOL", asset.assetName.capitalize())
-            mfragment.activity?.startActivity(intent)
-        }
 
         return view
     }
